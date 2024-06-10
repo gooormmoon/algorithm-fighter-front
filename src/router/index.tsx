5c2bd55cab447274338fbc4ba78e90d1e596d238
hwangheetae <tejaia@naver.com>
2024-06-10 22:04:05 +0900

    feat: mypage 기본 UI 생성

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,10 +4,10 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import Layout from "../components/Layout";
-
+import MyPage from "../pages/mypage";
 const router = (
   <Route path="/" element={<Layout />}>
-    <Route />
+    <Route path="/mypage" element={<MyPage />} />
   </Route>
 );
 const rootRouter = createBrowserRouter(createRoutesFromElements(router));
