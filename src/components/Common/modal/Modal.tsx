75f0c77fc0da5588f670778d76146e942a6ddee8
nnevia <dbs00024@gmail.com>
2024-06-10 16:28:18 +0900

    feat: modal 컴포넌트 생성

--- src/components/Common/modal/Modal.tsx
+++ src/components/Common/modal/Modal.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { useState } from 'react';
 
-function Modal() {
-  return <div>Modal</div>;
-}
+export const Modal = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Modal;
+  const openModalHandler = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <button
+        onClick={openModalHandler}
+        className=" py-3 px-6 rounded-lg cursor-pointer"
+      >
+        {isOpen ? 'Opened!' : 'event'}
+      </button>
+
+      {isOpen && (
+        <div
+          className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50"
+          onClick={openModalHandler}
+        >
+          <div
+            className="bg-white p-6 rounded-lg w-96"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <button
+              onClick={openModalHandler}
+              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto flex-justify-end dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              x
+            </button>
+            <div className="mt-6 text-lg text-[#213363]">
+              Goormdal
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
