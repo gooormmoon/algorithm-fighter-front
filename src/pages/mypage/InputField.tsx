0bde4642bfcfd2660843fc94615a64924bbda5e1
hwangheetae <tejaia@naver.com>
2024-06-11 17:32:39 +0900

    refactor:mypage

---
+++ src/pages/mypage/InputField.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Input from "../../components/Common/input/Input";
+
+interface InputFieldProps {
+  label: string;
+  type: string;
+  placeholder: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  errorText?: string;
+}
+
+const InputField: React.FC<InputFieldProps> = ({
+  label,
+  type,
+  placeholder,
+  value,
+  onChange,
+  errorText,
+}) => (
+  <div className="flex items-center mb-4">
+    <label className="block text-gray-700 w-32">{label}</label>
+    <Input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      name="test"
+      size="small"
+      errorText={errorText}
+    />
+  </div>
+);
+
+export default InputField;
