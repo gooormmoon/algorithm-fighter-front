5f99101fa7e483d2bb512862211cee13ae277c10
Jinju Seo <wjbbdq1223@naver.com>
2024-06-11 17:40:08 +0900

    fix: 프로필 배경색 수정

--- src/components/Layout/Header/Profile/index.tsx
+++ src/components/Layout/Header/Profile/index.tsx
@@ -1,28 +1,38 @@
-import React from 'react';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { sizing } from '@mui/system';
-import {Link} from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { sizing } from "@mui/system";
+import { Link } from "react-router-dom";
 const Profile = () => {
+  //   const [theme, setTheme] = useState(localStorage.getItem("theme"));
+  //   useEffect(() => {
+  //     setTheme(localStorage.getItem("theme"));
+  //   }, [localStorage.getItem("theme")]);
   return (
-    <section className={`w-[300px] h-[360px] absolute top-16 right-4 bg-white p-4 shadow-md  ${localStorage.theme==="dark" ? "bg-[#213363] text-white border-white":"bg-white text-[#213363] border-[#213363]"} `}>
-        <ul className='flex flex-col   w-full h-full items-center '>
-            {/* <img/> */}
-            <li className='w-full h-2/3 flex flex-col gap-4 border-b-2 border-gray-400 text-left'>
-                <div>   
-                <AccountCircleIcon fontSize="large"/>
-                </div>
-                <p className='text-xl'>
-                    구름달님
-                </p>
-                <p>
-                    프론트엔드 웹 풀스택
-                </p>
-            </li>
-            <li className='w-full h-12 flex items-center text-lg  border-b-2 border-gray-400 text-left '><Link to="/setting">설정</Link></li>
-            <li className='w-full  h-12 flex items-center text-lg  border-b-2 border-gray-400 text-left'><Link to="/logout">로그아웃</Link></li>
-        </ul>
+    <section
+      className={`w-[300px] h-[360px] absolute top-16 right-4  p-4 shadow-md  ${
+        localStorage.getItem("theme") === "dark"
+          ? "bg-[#213363] text-white border-white"
+          : "bg-white text-[#213363] border-[#213363]"
+      } `}
+    >
+      <ul className="flex flex-col   w-full h-full items-center ">
+        {/* <img/> */}
+        <li className="w-full h-2/3 flex flex-col gap-4 border-b-2 border-gray-400 text-left">
+          <div>
+            <AccountCircleIcon fontSize="large" />
+          </div>
+          <p className="text-xl">구름달님</p>
+          <p>프론트엔드 웹 풀스택</p>
+        </li>
+        <li className="w-full h-12 flex items-center text-lg  border-b-2 border-gray-400 text-left ">
+          <Link to="/setting">설정</Link>
+        </li>
+        <li className="w-full  h-12 flex items-center text-lg  border-b-2 border-gray-400 text-left">
+          <Link to="/logout">로그아웃</Link>
+        </li>
+      </ul>
     </section>
-  )
-}
+  );
+};
 
 export default Profile;
--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,56 +1,63 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import ForumIcon from '@mui/icons-material/Forum';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import LightModeIcon from '@mui/icons-material/LightMode';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import Profile from './Profile';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ForumIcon from "@mui/icons-material/Forum";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import Profile from "./Profile";
 
 const Header = () => {
-  const [showProfile,setShowProfile] = useState(false);
+  const [showProfile, setShowProfile] = useState(false);
   const [theme, setTheme] = useState("light");
   const toggleTheme = () => {
-    setTheme(theme === "light"? "dark" : "light");
-    localStorage.theme = theme === "light"? "dark" : "light";
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
   };
-  
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
   return (
-    <header className={`w-full h-[70px] flex justify-between items-center gap-12 p-4 shadow-xl ${localStorage.theme==="dark" ? "bg-[#213363] text-white border-white":"bg-white text-[#213363] border-[#213363]"   }` } >
-
-      <ul className='w-4/5 h-full flex gap-12 items-center'>
-        <li className='mr-12'>
-          로고
+    <header
+      className={`w-full h-[70px] flex justify-between items-center gap-12 p-4 shadow-xl ${
+        theme === "dark"
+          ? "bg-[#213363] text-white border-white"
+          : "bg-white text-[#213363] border-[#213363]"
+      }`}
+    >
+      <ul className="w-4/5 h-full flex gap-12 items-center">
+        <li className="mr-12">로고</li>
+        <li>
+          <Link to="/">홈</Link>
         </li>
         <li>
-         <Link to="/">홈</Link> 
+          <Link to="/">게임</Link>
+          {/* 클릭하면 모달창나오게 */}
         </li>
         <li>
-         <Link to="/">게임</Link> 
-         {/* 클릭하면 모달창나오게 */}
+          <Link to="/myRepository">내 저장소</Link>
         </li>
         <li>
-         <Link to="/myRepository">내 저장소</Link> 
-        </li>
-        <li>
-         <Link to="/setting">설정</Link> 
+          <Link to="/setting">설정</Link>
         </li>
       </ul>
       <ul className="h-full flex gap-4 items-center">
         <li>구름달님(예시)</li>
-        <li><button onClick={()=>toggleTheme()}>
-          {theme==="light"?<LightModeIcon/>:<DarkModeIcon/>}
-          </button></li>
-        <li><button onClick={()=>setShowProfile((prev)=>!prev)}>
-        <AccountCircleIcon/>
+        <li>
+          <button onClick={() => toggleTheme()}>
+            {theme === "light" ? <LightModeIcon /> : <DarkModeIcon />}
           </button>
-          {showProfile && <Profile/>}
-          </li>
-        <li><ForumIcon/></li>
+        </li>
+        <li>
+          <button onClick={() => setShowProfile((prev) => !prev)}>
+            <AccountCircleIcon />
+          </button>
+          {showProfile && <Profile />}
+        </li>
+        <li>
+          <ForumIcon />
+        </li>
       </ul>
     </header>
-  )
-}
+  );
+};
 
-export default Header
-
-
+export default Header;
