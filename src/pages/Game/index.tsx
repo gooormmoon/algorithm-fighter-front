1a6b23bf20f516d574ada12bb8ce2c8f1ecb3228
Jinju Seo <wjbbdq1223@naver.com>
2024-06-12 17:54:40 +0900

    feat: 게임, 저장소 레이아웃 마우스 드래그로 영역 Resizing 가능 기능 추가

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -1,28 +1,49 @@
 import React, { useEffect, useState } from "react";
 
 const Game = () => {
-  const [isResizing, setIsResizing] = useState(false);
+  const [isResizingX, setIsResizingX] = useState(false);
+  const [isResizingY, setIsResizingY] = useState(false);
   const [initialX, setInitialX] = useState(0);
-  const [width, setWidth] = useState(window.innerWidth / 2);
-  const onMouseDown = (e: React.MouseEvent) => {
+  const [initialY, setInitialY] = useState(0);
+  const [width, setWidth] = useState(window.innerWidth / 3);
+  const [height, setHeight] = useState(window.innerHeight / 1.5);
+
+  const onMouseDownX = (e: React.MouseEvent) => {
     e.preventDefault();
-    setIsResizing(true);
+    setIsResizingX(true);
     setInitialX(e.clientX);
   };
-  const onMouseUp = () => {
-    setIsResizing(false);
+
+  const onMouseDownY = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setIsResizingY(true);
+    setInitialY(e.clientY);
   };
+
+  const onMouseUp = () => {
+    setIsResizingX(false);
+    setIsResizingY(false);
+  };
+
   const onMouseMove = (e: MouseEvent) => {
-    if (isResizing) {
+    if (isResizingX) {
       const newWidth = width + e.clientX - initialX;
       setInitialX(e.clientX);
-      if (newWidth >= 360 && newWidth <= 1800) {
+      if (newWidth >= 360 && newWidth <= 1600) {
         setWidth(newWidth);
       }
     }
+    if (isResizingY) {
+      const newHeight = height + e.clientY - initialY;
+      setInitialY(e.clientY);
+      if (newHeight >= 120 && newHeight <= 2600) {
+        setHeight(newHeight);
+      }
+    }
   };
+
   useEffect(() => {
-    if (isResizing) {
+    if (isResizingX || isResizingY) {
       document.addEventListener("mousemove", onMouseMove);
       document.addEventListener("mouseup", onMouseUp);
     } else {
@@ -33,21 +54,32 @@ const Game = () => {
       document.removeEventListener("mousemove", onMouseMove);
       document.removeEventListener("mouseup", onMouseUp);
     };
-  }, [isResizing]);
+  }, [isResizingX, isResizingY]);
+
   return (
-    <main className="w-full h-full flex">
-      <div className="h-full " style={{ width }}>
-        <section></section>
+    <main className="w-full h-full flex overflow-hidden">
+      <div className="h-full" style={{ width }}>
+        <section className="w-[340px] h-[140px] rounded-xl bg-gray-800 ">
+          {/* top-20 left-4 absolute  */}
+          프로필영역
+        </section>
+        <section className="w-full h-full bg-orange-300"></section>
       </div>
       <div
-        className="flex justify-center items-center w-[28px] bg-blue-300 cursor-col-resize hover:bg-blue-500"
-        onMouseDown={onMouseDown}
+        className="flex justify-center items-center w-[20px] bg-blue-300 cursor-col-resize hover:bg-blue-500"
+        onMouseDown={onMouseDownX}
       >
         ||
       </div>
-      <div className="w-[2000px] h-full flex flex-col">
-        <section className="w-full h-3/4 bg-yellow-300"></section>
-        <section className="w-full h-1/4 bg-green-300"></section>
+      <div className="flex-1 h-full flex flex-col overflow-hidden">
+        <section className="w-full bg-yellow-300" style={{ height }}></section>
+        <div
+          className="flex flex-col justify-center items-center w-full h-[24px]text-center bg-blue-300 cursor-row-resize hover:bg-blue-500"
+          onMouseDown={onMouseDownY}
+        >
+          ||
+        </div>
+        <section className="w-full flex-1 bg-green-300"></section>
       </div>
     </main>
   );
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,11 +4,18 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import Layout from "../components/Layout";
-import { LoginPage, RegisterPage, MyPage, GamePage } from "../pages";
+import {
+  LoginPage,
+  RegisterPage,
+  MyPage,
+  GamePage,
+  MyReposPage,
+} from "../pages";
 
 const router = (
   <Route path="/" element={<Layout />}>
-    <Route path="/game" element={<GamePage />} />
+    <Route path="/game" element={<GamePage />} />\
+    <Route path="/myRepository" element={<MyReposPage />} />
     <Route path="/login" element={<LoginPage />} />
     <Route path="/mypage" element={<MyPage />} />
     <Route path="/register" element={<RegisterPage />} />
