9cb4e2164e238e77f4468df32d643ca21b766086
hwangheetae <tejaia@naver.com>
2024-06-12 17:32:14 +0900

    feat/코드에디터-GRTEAM-125

---
+++ src/game/components/CodeEditor.tsx
@@ -0,0 +1,60 @@
+import React, { useRef } from "react";
+import { Editor, OnMount } from "@monaco-editor/react";
+import { useState } from "react";
+import { CODE_SNIPPETS } from "../Constants";
+import Button from "../../components/Common/button/Button";
+import LanguageSelector from "./LanguageSelector";
+// import Output from "./Output";
+// import LanguageSelector from "./LanguageSelector";
+
+interface CodeEditorProps {}
+
+const CodeEditor: React.FC<CodeEditorProps> = () => {
+  const editorRef = useRef<any>(null);
+  const [value, setValue] = useState<string>("");
+  const [language, setLanguage] = useState<string>("javascript");
+  const [theme, setTheme] = useState<string>("vs-light");
+
+  const onMount: OnMount = (editor) => {
+    editorRef.current = editor;
+    editor.focus();
+  };
+
+  const onSelect = (language: string) => {
+    setLanguage(language);
+    setValue(CODE_SNIPPETS[language]);
+  };
+
+  //테마 전역 설정 전 테스트
+  const themeClick = () => {
+    setTheme((prevTheme) =>
+      prevTheme === "vs-light" ? "vs-dark" : "vs-light"
+    );
+  };
+
+  return (
+    <>
+      <LanguageSelector language={language} onSelect={onSelect} />
+      <Button
+        type="button"
+        size={"medium_small_radius"}
+        onClick={themeClick}
+        color="secondary"
+        textColor="primary_font"
+        name={"테마 바꾸기"}
+      ></Button>
+      <Editor
+        width={"60%"}
+        height="75vh"
+        theme={theme}
+        language={language}
+        defaultValue={CODE_SNIPPETS[language]}
+        onMount={onMount}
+        value={value}
+        onChange={(value) => setValue(value || "")}
+      ></Editor>
+    </>
+  );
+};
+
+export default CodeEditor;
