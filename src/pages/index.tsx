9b1ef3c9f6ff9c59bca16e952ae5f8f820bd2159
sora <yejh00426@gmail.com>
2024-06-18 11:35:06 +0900

    Feat/Main_Layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
 export { default as RegisterPage } from "./Auth/Register";
 export { default as LoginPage } from "./Auth/Login";
 export { default as MyPage } from "./MyPage";
+export { default as MainPage } from "./MainPage";
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,14 +4,16 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import Layout from "../components/Layout";
-import { LoginPage, RegisterPage, MyPage } from "../pages";
+import { LoginPage, RegisterPage, MyPage, MainPage } from "../pages";
 
 const router = (
   <Route path="/" element={<Layout />}>
+    <Route index element={<MainPage />} />
     <Route path="/login" element={<LoginPage />} />
     <Route path="/mypage" element={<MyPage />} />
     <Route path="/register" element={<RegisterPage />} />
   </Route>
 );
+
 const rootRouter = createBrowserRouter(createRoutesFromElements(router));
 export default rootRouter;
