6b3d2b9b801541ade78d3eda94aca4360aa865af
sora <yejh00426@gmail.com>
2024-06-18 17:39:26 +0900

    Feat/Wait_Layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,3 +6,4 @@ export { default as GamePage } from "./Game";
 export { default as MyReposPage } from "./MyRepository";
 export { default as GameProblem } from "./Game/GameProblem";
 export { default as MyPageRead } from "./MyPage/MyPageRead";
+export { default as Wait } from "./Wait"
