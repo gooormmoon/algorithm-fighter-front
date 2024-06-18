19bdb31d5fc0c2a58fdefdbbfc913e713d120c3a
nnevia <dbs00024@gmail.com>
2024-06-19 02:05:26 +0900

    fix: chatUI

--- src/pages/Wait/index.tsx
+++ src/pages/Wait/index.tsx
@@ -13,24 +13,27 @@ const Wait: React.FC = () => {
   const [selectedNumber, setSelectedNumber] = useState("10 minute");
 
   const navigate = useNavigate();
+  const handleClick = () => navigate('/');
 
   return (
     <main className={styles.layout}>
       <section className={styles.waitContainer}>
         <div className={styles.waitHeader}>
-          <Link to="/" className="w-1/5">
-            뒤로가기
-          </Link>
+          <div className={styles.buttons}>
+        <button className="font-bold text-[20px] text-white" onClick={handleClick}>
+          뒤로가기
+          </button>
+          </div>
           <h1 className={styles.title}>게임 방 제목</h1>
-          <div className="w-1/5 flex justify-center items-center gap-4">
-            <div className="w-2/3 h-full ">
+          <div className={styles.controls}>
+            <div className={styles.radioWrapper}>
               <RadioButton
                 selectedValue={selectedDifficulty}
                 onChange={(value) => setSelectedDifficulty(value)}
               />
             </div>
             <Dropdown
-              color={"text-secondary bg-white rounded flex "}
+              color={"text-secondary bg-white rounded flex font-bold"}
               options={numberOptions}
               selectedValue={selectedNumber}
               onChange={(value) => setSelectedNumber(value)}
@@ -47,22 +50,17 @@ const Wait: React.FC = () => {
             <CompetitorProfile />
           </div>
         </div>
-        <div className={styles.container}>
-          <div className={styles.box}>
+        <section className={styles.container}>
+          <div className={cx(styles.chatbox,styles.right)}>
             <Chat />
           </div>
-
-          <div className={cx(styles.box, styles.right)}>
-            <div className="w-full h-full flex justify-between gap-12">
-              <button className="w-1/2 h-full bg-primary text-[36px] text-white font-semibold rounded-2xl shadow-lg drop-shadow-lg hover:-translate-y-2 transition-all ease-in-out">
-                준비
-              </button>
-              <button className="w-1/2 h-full bg-tertiary text-[36px] text-white font-semibold  rounded-2xl shadow-lg drop-shadow-lg hover:-translate-y-2 transition-all ease-in-out">
-                게임 시작
-              </button>
+          <div className={cx(styles.right, styles.chatbox)}>
+            <div className={cx(styles.buttons, styles.container)}>
+              <button className={styles.readyButton}>준비</button>
+              <button className={styles.startButton}>게임 시작</button>
             </div>
           </div>
-        </div>
+        </section>
       </section>
     </main>
   );
