a44ad57f532db78eb32150579bd043fda6334972
hwangheetae <tejaia@naver.com>
2024-06-18 17:38:36 +0900

    feat:회원탈퇴 UI

---
+++ src/pages/MyPage/MembershipWithdrawalModal/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Modal } from "../../../components/Common";
+interface MembershipWithdrawalModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const MembershipWithdrawalModal: React.FC<MembershipWithdrawalModalProps> = ({
+  isOpen,
+  onClose,
+}) => {
+  // const { theme } = useTheme();
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} size="medium">
+      <div className="text-center">
+        <h2 className="text-xl font-bold mb-4">회원탈퇴</h2>
+        <p className="mb-4">정말로 회원탈퇴를 하시겠습니까?</p>
+        <div className="flex justify-center space-x-4">
+          <button
+            onClick={onClose}
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
+          >
+            취소
+          </button>
+          <button
+            onClick={() => {
+              //
+              onClose();
+            }}
+            className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+          >
+            회원탈퇴
+          </button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default MembershipWithdrawalModal;
--- src/pages/MyPage/MyPageRead/index.tsx
+++ src/pages/MyPage/MyPageRead/index.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../../../components/Common";
 import ImageUpload from "../MyPageUpdate/ImageUpload";
 import InputField from "../MyPageUpdate/InputField";
 import TextAreaField from "../MyPageUpdate/TextAreaField";
+import MembershipWithdrawalModal from "../MembershipWithdrawalModal";
+
 const MyPageRead = () => {
   const exampleData = {
     id: "JohnDoe@example.com",
@@ -21,102 +23,118 @@ const MyPageRead = () => {
     navigate(-1);
   };
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <div className="bg-transparent p-6 rounded-lg  max-w-2xl shadow-2xl drop-shadow-2xl">
-      <div className="flex justify-center mb-4">
-        <ImageUpload profileImage={exampleData.profileImageUrl} />
-      </div>
-      <div className="flex items-center text-center w-[488px] mb-4">
-        <div className="block w-32">id</div>
-        <div className="py-2 w-[360px]">{exampleData.id}</div>
-      </div>
-      <div className="flex items-center text-center w-[488px] mb-4">
-        <div className="block w-32">name</div>
-        <div className="py-2 w-[360px]">{exampleData.name}</div>
-      </div>
-      <div className="flex items-center text-center w-[488px] mb-4">
-        <div className="block w-32">nickname</div>
-        <div className="py-2 w-[360px]">{exampleData.nickname}</div>
-      </div>
-      <div className="flex items-center text-center w-[488px] mb-4">
-        <div className="block w-32">description</div>
-        <div className="py-2 w-[360px]">{exampleData.description}</div>
-      </div>
-      <div className="flex items-center text-center w-[488px] mb-4">
-        <div className="block w-32">createdDate</div>
-        <div className="py-2 w-[360px]">{exampleData.createdDate}</div>
-      </div>
-      <div className="flex items-center text-center w-[488px] mb-4">
-        <div className="block w-32">loginDate</div>
-        <div className="py-2 w-[360px]">{exampleData.loginDate}</div>
-      </div>
+    <>
+      <div className="bg-transparent p-6 rounded-lg  max-w-2xl shadow-2xl drop-shadow-2xl">
+        <div className="flex justify-center mb-4">
+          <ImageUpload profileImage={exampleData.profileImageUrl} />
+        </div>
+        <div className="flex items-center text-center w-[488px] mb-4">
+          <div className="block w-32">id</div>
+          <div className="py-2 w-[360px]">{exampleData.id}</div>
+        </div>
+        <div className="flex items-center text-center w-[488px] mb-4">
+          <div className="block w-32">name</div>
+          <div className="py-2 w-[360px]">{exampleData.name}</div>
+        </div>
+        <div className="flex items-center text-center w-[488px] mb-4">
+          <div className="block w-32">nickname</div>
+          <div className="py-2 w-[360px]">{exampleData.nickname}</div>
+        </div>
+        <div className="flex items-center text-center w-[488px] mb-4">
+          <div className="block w-32">description</div>
+          <div className="py-2 w-[360px]">{exampleData.description}</div>
+        </div>
+        <div className="flex items-center text-center w-[488px] mb-4">
+          <div className="block w-32">createdDate</div>
+          <div className="py-2 w-[360px]">{exampleData.createdDate}</div>
+        </div>
+        <div className="flex items-center text-center w-[488px] mb-4">
+          <div className="block w-32">loginDate</div>
+          <div className="py-2 w-[360px]">{exampleData.loginDate}</div>
+        </div>
 
-      <form>
-        <InputField
-          label="id"
-          type="text"
-          placeholder="id"
-          value={exampleData.id}
-          disabled={true}
-        />
+        <form>
+          <InputField
+            label="id"
+            type="text"
+            placeholder="id"
+            value={exampleData.id}
+            disabled={true}
+          />
 
-        <InputField
-          label="이름"
-          type="text"
-          placeholder="이름"
-          value={exampleData.name}
-          disabled={true}
-        />
-        <InputField
-          label="닉네임"
-          type="text"
-          placeholder="닉네임"
-          value={exampleData.nickname}
-          disabled={true}
-        />
+          <InputField
+            label="이름"
+            type="text"
+            placeholder="이름"
+            value={exampleData.name}
+            disabled={true}
+          />
+          <InputField
+            label="닉네임"
+            type="text"
+            placeholder="닉네임"
+            value={exampleData.nickname}
+            disabled={true}
+          />
 
-        <TextAreaField
-          label="소개"
-          placeholder="소개"
-          value={exampleData.description}
-          disabled={true}
-        />
-        {/* description */}
-        <InputField
-          label="회원가입 날짜"
-          type="text"
-          placeholder="회원가입 날짜"
-          value={exampleData.createdDate}
-          disabled={true}
-        />
-        <InputField
-          label="마지막 로그인 "
-          type="text"
-          placeholder="마지막 로그인 "
-          value={exampleData.loginDate}
-          disabled={true}
-        />
-      </form>
+          <TextAreaField
+            label="소개"
+            placeholder="소개"
+            value={exampleData.description}
+            disabled={true}
+          />
+          {/* description */}
+          <InputField
+            label="회원가입 날짜"
+            type="text"
+            placeholder="회원가입 날짜"
+            value={exampleData.createdDate}
+            disabled={true}
+          />
+          <InputField
+            label="마지막 로그인 "
+            type="text"
+            placeholder="마지막 로그인 "
+            value={exampleData.loginDate}
+            disabled={true}
+          />
+        </form>
 
-      <div className="m-2 p-2 flex flex-col gap-2 items-center">
-        <Button
-          type="button"
-          size="medium_big_radius"
-          color="secondary"
-          textColor="primary_font"
-          name="수정하기"
-          onClick={() => navigate("/mypageUpdate")}
-        />
-        <Button
-          type="button"
-          size="medium_big_radius"
-          color="secondary_border"
-          textColor="secondary_color_font"
-          name="뒤로가기"
-          onClick={handleCancel}
-        />
+        <div className="m-2 p-2 flex flex-col gap-2 items-center">
+          <Button
+            type="button"
+            size="medium_big_radius"
+            color="secondary"
+            textColor="primary_font"
+            name="수정하기"
+            onClick={() => navigate("/mypageUpdate")}
+          />
+          <Button
+            type="button"
+            size="medium_big_radius"
+            color="secondary_border"
+            textColor="secondary_color_font"
+            name="뒤로가기"
+            onClick={handleCancel}
+          />
+        </div>
+        <button
+          className="flex text-gray-500 underline ml-auto"
+          onClick={() => {
+            setIsModalOpen(true);
+          }}
+        >
+          회원탈퇴
+        </button>
       </div>
-    </div>
+      <MembershipWithdrawalModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
+    </>
   );
 };
 
