5c4ce841b44f1e03ff56b9b740f8b6cc850d46d6
Jinju Seo <wjbbdq1223@naver.com>
2024-06-19 23:50:01 +0900

    feat: stompjs 연결 관련 api

--- src/api/Game/index.ts
+++ src/api/Game/index.ts
@@ -35,4 +35,56 @@
 // export const sendMessage = (message: any) => {
 //   stompClient?.send("app/game", {}, JSON.stringify(message));
 // };
+
+//MEMO: main에서 호출될예정
+import { Client } from "@stomp/stompjs";
+import { getTokens } from "../../utils";
+import StompJS from "@stomp/stompjs";
+// export const getClient = () => {
+//   return new Client({
+//     brokerURL: "ws://localhost:8080/game",
+//     connectHeaders: {
+//       Authorization: `Bearer ${getTokens()}`,
+//     },
+//     debug: (str: string) => {
+//       console.log(str);
+//     },
+//     reconnectDelay: 5000, //자동 재 연결
+//     heartbeatIncoming: 4000,
+//     heartbeatOutgoing: 4000,
+//   });
+// };
+const client = new StompJS.Client({
+  brokerURL: "/api/ws",
+  connectHeaders: {
+    login: "user",
+    passcode: "password",
+  },
+  debug: function (str) {
+    console.log(str);
+  },
+  reconnectDelay: 5000, //자동 재 연결
+  heartbeatIncoming: 4000,
+  heartbeatOutgoing: 4000,
+});
+client.onConnect = function (frame) {};
+
+client.onStompError = function (frame) {
+  console.log("Broker reported error: " + frame.headers["message"]);
+  console.log("Additional details: " + frame.body);
+};
+//클라이언트 활성화
+client.activate();
+//클라이언트 비활성화
+client.deactivate();
+
+//메세지 보내기
+client.publish({
+  destination: "/topic/general",
+  body: "Hello world",
+  headers: { priority: "9" },
+});
+//메세지 받기
+const callback = () => {};
+const subscription = client.subscribe("/queue/test", callback);
 export {};
