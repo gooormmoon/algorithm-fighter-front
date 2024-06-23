7d584e30bd3bd386fc04ca520eba108dd02cdc14
nnevia <dbs00024@gmail.com>
2024-06-24 01:16:21 +0900

    toast수정

--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -136,7 +136,6 @@ const Login = () => {
             setGameClient(gameClient);
 
             navigate("/");
-            toast.success("로그인 성공!");
           }
         }
       }
@@ -158,51 +157,51 @@ const Login = () => {
         className={`w-full h-[100vh] flex flex-col justify-start items-center gap-[6vh]
         ${"bg-gradient-to-br from-[#327074] via-[#2a4e7d] to-[#22264C] text-white "}`}
       >
-        <div className="flex justify-start w-full px-4">
+        <div className='flex justify-start w-full px-4'>
           <img
             src={Lightmode_logo}
-            alt="Lightmode_logo"
-            className="w-[12vh] h-[12vh] object-contain"
+            alt='Lightmode_logo'
+            className='w-[12vh] h-[12vh] object-contain'
           />
         </div>
         <form
-          className="w-[52vh] h-[60vh] gap-4 flex flex-col justify-center items-center  rounded-md  bg-transparent "
+          className='w-[52vh] h-[60vh] gap-4 flex flex-col justify-center items-center  rounded-md  bg-transparent '
           onSubmit={onSubmit}
         >
-          <h1 className=" text-[68px] font-semibold">Login</h1>
+          <h1 className=' text-[68px] font-semibold'>Login</h1>
 
           <Input
-            type="email"
-            placeholder="Email"
+            type='email'
+            placeholder='Email'
             value={form?.email}
             onChange={onChange}
-            name="email"
-            size="large"
+            name='email'
+            size='large'
             border={false}
           />
           <Input
-            type="password"
-            placeholder="Password"
+            type='password'
+            placeholder='Password'
             value={form?.password}
             onChange={onChange}
-            name="password"
-            size="large"
+            name='password'
+            size='large'
             border={false}
           />
           <Button
-            type="submit"
-            size="large_radius"
-            color="secondary"
-            textColor="primary_font"
-            name="로그인"
+            type='submit'
+            size='large_radius'
+            color='secondary'
+            textColor='primary_font'
+            name='로그인'
           />
-          <ul className="mt-4 flex gap-2 text-sm">
-            <li className="cursor-pointer">아이디 찾기</li>
+          <ul className='mt-4 flex gap-2 text-sm'>
+            <li className='cursor-pointer'>아이디 찾기</li>
             <li>|</li>
-            <li className="cursor-pointer">비밀번호 찾기</li>
+            <li className='cursor-pointer'>비밀번호 찾기</li>
             <li>|</li>
             <li
-              className="font-semibold cursor-pointer"
+              className='font-semibold cursor-pointer'
               onClick={() => navigate("/register")}
             >
               회원가입
