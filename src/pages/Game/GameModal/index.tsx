fdb3508a8ea6a8c8d756042fa70e256c178cfda2
nnevia <dbs00024@gmail.com>
2024-06-24 03:51:58 +0900

    fix:TimeoutModal추가 VictoryModal 폭죽 zindex 수정

--- src/pages/Game/GameModal/index.tsx
+++ src/pages/Game/GameModal/index.tsx
@@ -3,3 +3,4 @@ export { default as CreateModal } from "./CreateModal";
 export { default as VictoryModal } from "./VictoryModal";
 export { default as DefeatModal } from "./DefeatModal";
 export { default as TestCaseModal } from "./TestCaseModal";
+export { default as TimeoutModal } from "./TimeoutModal";
