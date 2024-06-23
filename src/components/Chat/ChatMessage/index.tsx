0f38cf7b6cd4cb015268291ef6525988342dfabc
nnevia <dbs00024@gmail.com>
2024-06-23 21:36:04 +0900

    feat: chat Nickname 변경

--- src/components/Chat/ChatMessage/index.tsx
+++ src/components/Chat/ChatMessage/index.tsx
@@ -1,18 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ProfileIcon from "../../Common/ProfileIcon";
 import { GameMessage } from "..";
+import { getUser } from "../../../api/Users";
 
 const ChatMessage = ({ message }: { message: GameMessage }) => {
+  const [userName, setUserName] = useState<string | null>(null);
+  const [profileImageUrl, setProfileImageUrl] = useState<string | null>(null);
+  useEffect(() => {
+    getUser(message.sender_id).then((response) => {
+      setUserName(response.data.nickname);
+      setProfileImageUrl(response.data.profile_image_url);
+    });
+  }, [message.sender_id]);
   return (
     <div className={`text-primary flex justify-start gap-1 items-center`}>
       <div
         className={`w-[92px] flex justify-center items-center p-1 rounded gap-1`}
       >
-        <ProfileIcon size='small' />
-        {/* <span className="">{nickname}</span> */}
+        {profileImageUrl && <ProfileIcon size='small' src={profileImageUrl} />}
+        <span className=''>{userName}</span>
         <span>:</span>
       </div>
-
       <span className=''>{message.content}</span>
     </div>
   );
